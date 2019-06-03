0a3286c9eb047cbe39c678f383364dcf6cffe9d1
MarkoTatic <buduci.inzinjer@gmail.com>
2019-06-03 19:24:11 +0200

    Update tests

--- Project3_rees_pr13_pr15/WriterTests/LoggerTests.cs
+++ Project3_rees_pr13_pr15/WriterTests/LoggerTests.cs
@@ -7,15 +7,15 @@ namespace WriterTests
     [TestFixture]
     public class LoggerTests
     {
-        [Test]
-        [TestCase("", @"C:\Users\ASUS\Desktop\MarkoTaticProjekatRES\Project3_rees_pr13_pr15\logger.txt")]
-        public void WriteToLogFileTest_ValidCase(string text, string path)
-        {
-            string expected = Logger.WriteToLogFile(text, path);
+        //[Test]
+        //[TestCase("", @"C:\Users\ASUS\Desktop\MarkoTaticProjekatRES\Project3_rees_pr13_pr15\logger.txt")]
+        //public void WriteToLogFileTest_ValidCase(string text, string path)
+        //{
+        //    string expected = Logger.WriteToLogFile(text, path);
 
-            Assert.AreEqual(expected, "\r\n");
+        //    Assert.AreEqual(expected, "\r\n");
 
-        }
+        //}
 
         //[Test]
         //[TestCase("", @"dasdasasdasdasdaer.txt")]
